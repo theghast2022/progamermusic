bfbb764c3c0d37d0ff1f931926d69f7229f479ce
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2019-02-01 06:22:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -238,7 +238,7 @@ member.addRole(member.guild.roles.find(`name`, ar[member.guild.id].role)).catch(
 })
 
 client.on('message', message => { 
-  var whitelisted = "اكتب ايديك هنا"// ايديك
+  var whitelisted = "اكتب ايديك هنا//"388357326614167563
   var sender = message.author
 
 if(!message.guild) return
